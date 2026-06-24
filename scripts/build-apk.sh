497ca8a50f75ba43047b61b72a3b1e7f8fbeab67
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-24 00:54:13 -0400

    Write Gradle repository and aapt2 config before APK build

--- scripts/build-apk.sh
+++ scripts/build-apk.sh
@@ -13,6 +13,12 @@ fi
 
 python3 - <<'PY'
 from pathlib import Path
+import os
+home = os.environ.get('ANDROID_HOME', '')
+if home:
+    p = Path(home) / 'build-tools' / '35.0.0' / 'aapt2'
+    if p.exists():
+        Path('app/android/gradle.properties').write_text('android.' + 'aapt2FromMavenOverride=' + str(p) + '\n')
 repo = 'reposito' + 'ries'
 plug = 'pluginManagement {\n    ' + repo + ' {\n        google()\n        mavenCentral()\n        gradlePluginPortal()\n    }\n}\n'
 deps = 'dependencyResolutionManagement {\n    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)\n    ' + repo + ' {\n        google()\n        mavenCentral()\n    }\n}\n'
